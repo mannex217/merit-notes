2aa1a15f3c002302a549f4f5d55b7401660a0311
Merit Student <student@meac.local>
2018-08-27 11:40:19 -0600

    I took my notes

--- unit1/data-types.py
+++ unit1/data-types.py
@@ -16,23 +16,38 @@ multiline comment
 
 # *** DATA TYPE: INTEGER ***
 # TODO: An integer number (no decimal)
-
+integer = 5
+print(integer)
+print(type(integer))
 
 # *** DATA TYPE: FLOAT ***
 # TODO: A decimal number
-
+float = 1.79
+print(float)
+print(type(float))
 
 # *** DATA TYPE: STRING ***
 # TODO: A string of characters enclosed in quotes
-
+word = "this is a string"
+print(word)
+print(type(word))
 
 # *** TYPE CASTING ***
 # This converts one type to another
 
 # TODO: Cast float to int
+float_num = 5.5
+int_num = int(float_num)
+print(int_num)
+print(type(int_num))
 
 # TODO: Cast int to string
-
+number = 5
+print("this is the number " + str(number))
+print(type(number))
 # TODO: Cast number string to int
-
+numb = "5"
+total = int(numb) + 2
+print(total)
+print(type(total))
 # TODO: Input demo (str to float)
